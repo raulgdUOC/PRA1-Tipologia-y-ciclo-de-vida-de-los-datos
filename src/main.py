6ed61ce33e07afb5db711a0b8de8ff5616fac9ad
raulgdUOC <131053997+raulgdUOC@users.noreply.github.com>
2023-04-25 23:37:11 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,19 +3,19 @@ import sys
 from imdbClassDataSet import ImdbDataSet
 
 def main():
+    # Catching the data wrote in console
     parser = argparse.ArgumentParser()
     parser.add_argument('--type_', type=str, default='movie', help='Elige el tipo de contenido')
     parser.add_argument('--genre', type=str, default='comedy', help='Elige el tipo de genero del contenido')
 
     args = parser.parse_args()
 
-    scraper = ImdbDataSet(args.type_, args.genre)
-    scraper(200)
-    scraper.save_dataset()
+    # Data is collected through ImdbDataSet
+    scraper = ImdbDataSet(type_=args.type_, genre=args.genre)  # Instantiate the class
+    scraper(200)  # Getting the information from the first 200 pages
+    scraper.save_dataset()  # Saving the dataset
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
-
-
-
-
+    

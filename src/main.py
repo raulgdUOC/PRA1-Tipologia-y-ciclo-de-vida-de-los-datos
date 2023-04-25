0a86b8da8e010ad9c2f964db0dd358c0591c2d5b
raulgdUOC <131053997+raulgdUOC@users.noreply.github.com>
2023-04-25 19:28:00 +0200

    Create main.py

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ def main():
     args = parser.parse_args()
 
     scraper = ImdbDataSet(args.type_, args.genre)
-    scraper(10000)
+    scraper(200)
     scraper.save_dataset()
 
 if __name__=='__main__':
